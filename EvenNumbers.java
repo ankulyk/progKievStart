a515ff3184df1b665edb74c9796e95b6ee4d0aa2
ankulyk <akm_kan@ukr.net>
2016-06-30 00:25:09 +0300

    Create EvenNumbers.java

---
+++ EvenNumbers.java
@@ -0,0 +1,12 @@
+package progKievStart.Lesson4;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int a = 1, b = 50;
+
+        for (int i = a; i < b; i++) {
+            i++;
+            System.out.print(i + " ");
+        }
+    }
+}
