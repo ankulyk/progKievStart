487f9395679765921557e0f443b9a308ef2f7959
ankulyk <akm_kan@ukr.net>
2016-06-30 00:22:08 +0300

    Create FromTwentyToMinusFourty.java

---
+++ FromTwentyToMinusFourty.java
@@ -0,0 +1,14 @@
+package progKievStart.Lesson4;
+
+public class FromTwentyToMinusFourty {
+    public static void main(String[] args) {
+        int a = 20, b = -40;
+
+
+        for (int i = b; i <= a; i++) {
+            System.out.print(i + " ");
+        }
+
+
+    }
+}
