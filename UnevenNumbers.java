da16647eca218ffb83e59ae7caee1dff14383679
ankulyk <akm_kan@ukr.net>
2016-06-30 00:25:44 +0300

    Create UnevenNumbers.java

---
+++ UnevenNumbers.java
@@ -0,0 +1,12 @@
+package progKievStart.Lesson4;
+
+public class UnevenNumbers {
+    public static void main(String[] args) {
+        int a = 100, b = 70;
+
+        for (int i = a; i > b; i--) {
+            i--;
+            System.out.print(i + " ");
+        }
+    }
+}
