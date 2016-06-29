f6abe02f2b694d8a38ec326e06d790e6f6c1d207
ankulyk <akm_kan@ukr.net>
2016-06-30 00:21:27 +0300

    Create FromTenToThirty.java

---
+++ FromTenToThirty.java
@@ -0,0 +1,11 @@
+package progKievStart.Lesson4;
+
+public class FromTenToThirty {
+    public static void main(String[] args) {
+        int a = 10, b = 30;
+
+        for(int i = a; i <= b; i++){
+            System.out.print(i+ " ");
+        }
+    }
+}
