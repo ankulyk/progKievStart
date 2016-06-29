7e9d37f5e13a10654cc18a906161dcaea7f23196
ankulyk <akm_kan@ukr.net>
2016-06-29 23:48:18 +0300

    Create MinTwoNumbers.java

---
+++ MinTwoNumbers.java
@@ -0,0 +1,13 @@
+package progKievStart.Lesson4;
+
+public class MinTwoNumbers {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+
+        if (a <= b) {
+            System.out.println("Min: " + a);
+        } else {
+            System.out.println("Min: " + b);
+        }
+    }
+}
