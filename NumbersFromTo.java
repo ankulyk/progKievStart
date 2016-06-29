1b6ba5b64eb953e03576009018cadd66df1e0ecb
ankulyk <akm_kan@ukr.net>
2016-06-30 00:22:58 +0300

    Create NumbersFromTo.java

---
+++ NumbersFromTo.java
@@ -0,0 +1,11 @@
+package progKievStart.Lesson4;
+
+public class NumbersFromTo {
+    public static void main(String[] args) {
+        int from = 10, to = 15;
+
+        for (int i = from; i <= to; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
