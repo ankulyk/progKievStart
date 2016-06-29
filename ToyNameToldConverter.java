13367d09a3dda293d3d754cc7b0d1489bd6dd10d
ankulyk <akm_kan@ukr.net>
2016-06-29 18:11:47 +0300

    Create ToyNameToldConverter.java

---
+++ ToyNameToldConverter.java
@@ -0,0 +1,33 @@
+package progKievStart.Lesson4;
+
+import java.util.Scanner;
+
+public class ToyNameToldConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter toyName: ");
+        String toyName = sc.nextLine();
+
+        int idToy;
+
+        switch (toyName) {
+            case "Car":
+                idToy = 0;
+                break;
+            case "Lego":
+                idToy = 1;
+                break;
+            case "Doll":
+                idToy = 2;
+                break;
+            case "Puzzle":
+                idToy = 3;
+                break;
+            default:
+                throw new IllegalArgumentException("We don't have this toyName.");
+        }
+        System.out.println("idToy: " + idToy);
+        sc.close();
+    }
+}
