5331dadf72660a651874152dc9af22f12f6c9b7c
ankulyk <akm_kan@ukr.net>
2016-07-12 13:44:34 +0300

    Update IsAgeChecker.java

--- IsAgeChecker.java
+++ IsAgeChecker.java
@@ -4,18 +4,16 @@ public class IsAgeChecker {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+        boolean isAge = true;
+        
         System.out.print("Enter age:");
         int age = sc.nextInt();
-
-        boolean isAge = true;
 
         if (age => 1 && age <= 120) {
             System.out.println(isAge);
         } else {
             System.out.println(!isAge);
         }
-
         sc.close();
     }
 }
